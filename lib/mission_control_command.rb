b729a287b914a229422851b8693a7df163a10a10
Michael Raimondi <zeno@pobox.com>
2013-05-24 11:37:45 -0400

    Add checked in time notification and checked out duration

--- lib/mission_control_command.rb
+++ lib/mission_control_command.rb
@@ -1,4 +1,7 @@
 require 'time'
+# require 'actionpack/lib/action_view/helpers/date_helper'
+require 'action_view'
+include ActionView::Helpers::DateHelper
 
 class MissionControlCommand
   def initialize(cmd)
@@ -15,16 +18,16 @@ class MissionControlCommand
         File.open(self.class.check_in_path, 'w') do |f|
           f.puts Time.now
         end
-        return 'You are checked in'
+        return "You are checked in as of #{Time.now.strftime('%l:%M %p')}"
       else
         return 'You are already checked in.'
       end
     elsif @cmd == 'out'
       if FileTest.exists?(self.class.check_in_path)
         time_in = Time.parse(File.read(self.class.check_in_path))
-        total_time = ((Time.now - time_in).to_i).to_s
+        total_time = distance_of_time_in_words_to_now(time_in, true)
         File.delete(self.class.check_in_path)
-        return "You are checked out. You were checked in for #{total_time}"
+        return "You are checked out.\nTotal time spent at Mission Control: #{total_time}"
       else
         return 'You are already checked out.'
       end
--- spec/mission_control_command_spec.rb
+++ spec/mission_control_command_spec.rb
@@ -9,78 +9,83 @@ describe MissionControlCommand do
   end
 
   context 'checking in' do
+  let(:cmd_in) { MissionControlCommand.new('in') }
+  let(:now) { Time.now.strftime('%l:%M %p') }
+
     it 'acknowledges my check in' do
-      cmd = MissionControlCommand.new('in')
-      expect(cmd.output).to include('You are')
+      expect(cmd_in.output).to include('You are')
     end
 
     it 'creates a check in file' do
-      cmd = MissionControlCommand.new('in')
-      check_in_path = cmd.class.check_in_path
+      check_in_path = cmd_in.class.check_in_path
       if FileTest.exists?(check_in_path)
         FileUtils.rm(check_in_path)
       end
 
-      cmd.output
+      cmd_in.output
       expect(check_in_path).to be_true
     end
 
     it 'timestamps the checkin file at the time of checkin' do
-      cmd = MissionControlCommand.new('in')
-      cmd.output
-      time = File.read(cmd.class.check_in_path).chomp
+      cmd_in.output
+      time = File.read(cmd_in.class.check_in_path).chomp
       expect(time).to eql((Time.now).to_s)
     end
 
     it 'throws a checkin error if you try to checkin twice' do
-      cmd = MissionControlCommand.new('in')
       cmd2 = MissionControlCommand.new('in')
-      cmd.output
+      cmd_in.output
       cmd2.output
       expect(cmd2.output).to include('You are already checked in.')
     end
 
     it 'does not change the timestamp if you try to check in again' do
-      cmd = MissionControlCommand.new('in')
-      cmd.output
-      initial_time = File.read(cmd.class.check_in_path).chomp
+      cmd_in.output
+      initial_time = File.read(cmd_in.class.check_in_path).chomp
       cmd2 = MissionControlCommand.new('in')
       cmd2.output
-      final_time = File.read(cmd.class.check_in_path).chomp
+      final_time = File.read(cmd_in.class.check_in_path).chomp
       expect(initial_time).to eql(final_time)
     end
+
+    context 'checking out' do
+      let(:cmd_out) { MissionControlCommand.new('out') }
+
+      it 'acknowledges my check out' do
+        expect(cmd_out.output).to include("You are checked out")
+      end
+
+      it 'deletes the check in file' do
+        check_in_path = cmd_out.class.check_in_path
+        expect(FileTest.exists?(check_in_path)).to eql(false)
+      end
+
+      it 'tells you how long you were checked in' do
+        cmd_in.output
+        time_in = Time.parse(File.read(cmd_in.class.check_in_path).chomp)
+        sleep(1)
+        out = cmd_out.output
+        expect(out).to include("5")
+      end
+
+      it 'includes "seconds"' do
+        cmd_in.output
+        time_in = Time.parse(File.read(cmd_in.class.check_in_path).chomp)
+        sleep(1)
+        out = cmd_out.output
+        total_time = (Time.now - time_in).to_i
+        expect(out).to include("seconds")
+      end
+
+      it 'throws a checkout error if you try to checkout twice' do
+        cmd2 = MissionControlCommand.new('out')
+        cmd_out.output
+        cmd2.output
+        expect(cmd2.output).to include('You are already checked out.')
+      end
+    end
+
   end
 
-  context 'checking out' do
-    it 'acknowledges my check out' do
-      cmd = MissionControlCommand.new('out')
-      expect(cmd.output).to include("You are checked out")
-    end
-
-    it 'deletes the check in file' do
-      cmd = MissionControlCommand.new('out')
-      check_in_path = cmd.class.check_in_path
-      expect(FileTest.exists?(check_in_path)).to eql(false)
-    end
-
-    it 'tells you how long you were checked in' do
-      cmd_in = MissionControlCommand.new('in')
-      cmd_in.output
-      time_in = Time.parse(File.read(cmd_in.class.check_in_path).chomp)
-      sleep(1)
-      cmd_out = MissionControlCommand.new('out')
-      out = cmd_out.output
-      total_time = (Time.now - time_in).to_i
-      expect(out).to include(total_time.to_s)
-    end
-
-    it 'throws a checkout error if you try to checkout twice' do
-      cmd = MissionControlCommand.new('out')
-      cmd2 = MissionControlCommand.new('out')
-      cmd.output
-      cmd2.output
-      expect(cmd2.output).to include('You are already checked out.')
-    end
-  end
 
 end
